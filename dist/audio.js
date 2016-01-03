e5ff631d9e48c8c5e93e3428afcf12d2b77e090c
Michael Bylstra <mbylstra@gmail.com>
2016-01-03 17:34:50 +1100

    cleaned up audio.js a little

--- dist/audio.js
+++ dist/audio.js
@@ -31,118 +31,63 @@ var unpackTuple2 = function(tuple2) {
 }
 
 
-
 if (DEBUG) {
      ITERATIONS = 0;
 }
 
 var elmGui = Elm.fullscreen(Elm.Gui);
-// var elmAudio = Elm.worker(Elm.ReactiveAudio);
-
-
 
 //expose Elm modules
 var Orchestrator = exposeElmModule(Elm.Orchestrator);
+var ReactiveAudio = exposeElmModule(Elm.ReactiveAudio);
 
 var updateGraph = function(initialAudioGraph, externalState) {
   return unpackTuple2(Orchestrator.updateGraph(initialAudioGraph)(externalState));
 }
-var ReactiveAudio = exposeElmModule(Elm.ReactiveAudio);
 
 var initialAudioGraph = Orchestrator.toDict(ReactiveAudio.audioGraph);
 var audioGraph = initialAudioGraph;
 
+var externalState = {
+  time: 0.0,
+  externalInputState: {
+    xWindowFraction: 0.0,
+    yWindowFraction: 0.0,
+    audioOn : false,
+  }
+}
+
+var monoBuffer = [];
+
+function fillBuffer() {
+  // console.log('audioGraph', audioGraph);
+  // console.log('externalState', externalState);
+  for (var i = 0; i < BUFFER_SIZE; i++) {
+    var result = updateGraph(audioGraph, externalState);
+    audioGraph = result[0];
+    monoBuffer[i] = result[1];
+  }
+}
+
+
 if (PROFILING) {
     console.log('start profiling');
-
     var i = ITERATIONS;
-    BUFFER_SIZE = 2000;
-    var monoBuffer = [];
-    for (var i = 0; i < BUFFER_SIZE; i++) {
-      var result = updateGraph(audioGraph, externalState);
-      audioGraph = result[0];
-      monoBuffer[i]= result[1];
-    }
+    fillBuffer();
     console.log(monoBuffer);
-    // elm.ports.latestBuffer.subscribe(function(buffer) {
-    //     if (PROFILING && DEBUG) {
-    //         console.log('buffer', buffer);
-    //     }
-    //     if (i > 0) {
-    //         elm.ports.requestBuffer.send(true);
-    //         i--;
-    //     } else {
-    //         endTime = window.performance.now();
-    //         millisElapsed = endTime - startTime;
-    //         secondsElapsed = millisElapsed / 1000.0;
-    //         console.log('secondsElapsed: ', secondsElapsed);
-    //         console.log('max allowed duration: ', MAX_ALLOWED_DURATION);
-    //         console.log('CPU percent:', (secondsElapsed / MAX_ALLOWED_DURATION) * 100.0);
-    //
-    //     }
-    // });
-
 } else {
 
-
-
-
-
-// type alias ExternalInputState =
-//     { xWindowFraction : Float
-//     , yWindowFraction : Float
-//     , audioOn : Bool
-//     }
-// type alias ExternalState =
-//     { time : Float
-//     , externalInputState : ExternalInputState
-//     }
-
-
-    var externalState = {
-      time: 0.0,
-      externalInputState: {
-        xWindowFraction: 0.0,
-        yWindowFraction: 0.0,
-        audioOn : false,
-      }
-    }
-
-    // var result = updateGraph(initialAudioGraph, externalState);
-    //expose Elm modules
-    // var Orchestorator = Elm.Orchestrator.make(elmAudio);
-
-
-    // var latestBuffer = [];
-
-
     elmGui.ports.outgoingUserInput.subscribe(function(userInput) {
-      // latestUserInput = userInput;
       externalState.externalInputState = userInput;
     });
-
-
-    // var Orchestrator = Elm.Orchestrator.make(elm);
-    // var ReactiveAudio = Elm.ReactiveAudio.make(elm);
-    // console.log('audioGraph', ReactiveAudio.audioGraph);
-
 
     var audioCtx = new AudioContext();
     source = audioCtx.createBufferSource();
     var scriptNode = audioCtx.createScriptProcessor(BUFFER_SIZE, 1, 1);
-//     var iirFilter = audioCtx.createIIRFilter();
-//     var lowpass = audioCtx.createBiquadFilter();
     scriptNode.onaudioprocess = function(audioProcessingEvent) {
-      // console.log('latest user input', latestUserInput);
         var outputBuffer = audioProcessingEvent.outputBuffer;
 
-        var monoBuffer = [];
-        for (var i = 0; i < BUFFER_SIZE; i++) {
-          var result = updateGraph(audioGraph, externalState);
-          audioGraph = result[0];
-          monoBuffer[i]= result[1];
-        }
-        // here we fill the buffer (and use same values for both channels)
+        fillBuffer();
 
         for (var channelNumber = 0; channelNumber < outputBuffer.numberOfChannels; channelNumber++) {
           var outputData = outputBuffer.getChannelData(channelNumber);
@@ -156,84 +101,7 @@ if (PROFILING) {
                   outputData[i] = value;
               }
           }
-
-// -- updateBufferState : UserInput -> BufferState -> BufferState
-// -- updateBufferState userInput prevBufferState =
-// --
-// --     let
-// --
-// --         externalInputState : ExternalInputState
-// --         externalInputState =
-// --             { xWindowFraction = toFloat userInput.mousePosition.x / toFloat userInput.windowDimensions.width
-// --             , yWindowFraction = toFloat userInput.mousePosition.y / toFloat userInput.windowDimensions.height
-// --             , audioOn = userInput.audioOn
-// --             }
-// --         -- _ = Debug.log "externalInputState: " externalInputState
-// --
-// --         time = prevBufferState.time + sampleDuration
-// --
-// --         -- frequency = 40.0 + (xWindowFraction * 10000.0) -- how do we pass this in?
-// --
-// --         initialGraph = prevBufferState.graph
-// -- {-         _ = Debug.log "sampleCuration" sampleDuration
-// --         _ = Debug.log "updateBufferState time" time -}
-// --
-// --
-// --         -- surely we can do this without having to manually create a counter?
-// --         -- we can just iterate over the last buffer, and ignore values
-// --
-// --         prevBuffer = prevBufferState.buffer
-// --
-// --         initialBufferState : BufferState
-// --         initialBufferState =
-// --             { time = time
-// --             , graph = initialGraph
-// --             , buffer = prevBuffer
-// --             , bufferIndex = 0
-// --             , externalInputState = externalInputState
-// --             }
-// --
-// --         -- we must expose this as a public function
-// --         updateForSample {time, graph, buffer, bufferIndex} =
-// --             let
-// --                 newTime  = time + sampleDuration
-// --                 externalState =
-// --                     { time = newTime
-// --                     , externalInputState = externalInputState
-// --                     }
-// -- --                 _ = Debug.log "udpateForSample newTime" newTime
-// --                 newBufferIndex = bufferIndex + 1
-// -- --                 _ = Debug.log "newBufferIndex" newBufferIndex
-// -- --                 _ = Debug.log "value" value
-// --             in
-// --                 if
-// --                     externalInputState.audioOn == True
-// --                 then
-// --                     let
-// --                         -- this is pretty much all elm will do
-// --                         (newGraph, value) = updateGraph graph externalState
-// --                     in
-// --                         -- this will be done in JS land
-// --                         { time  = newTime
-// --                         , graph = newGraph
-// --                         , buffer = Array.set newBufferIndex value buffer
-// --                         , bufferIndex = newBufferIndex
-// --                         , externalInputState =  externalInputState
-// --                         }
-// --                 else
-// --                     { time  = newTime
-// --                     , graph = graph
-// --                     , buffer = Array.set newBufferIndex 0.0 buffer
-// --                     , bufferIndex = newBufferIndex
-// --                     , externalInputState =  externalInputState
-// --                     }
-// --     in
-// --         foldn updateForSample initialBufferState bufferSize
     }
-
-
-
-
     source.connect(scriptNode);
     scriptNode.connect(audioCtx.destination);
     source.start();
--- src/ReactiveAudio.elm
+++ src/ReactiveAudio.elm
@@ -15,7 +15,7 @@ import Gui exposing(dummy)
 -- import Graphics.Element
 -- import Graphics.Input
 
-import Time exposing (Time, fps)
+-- import Time exposing (Time, fps)
 -- import Keyboard
 import AudioNodes exposing
 
@@ -65,31 +65,8 @@ type alias BufferState =
     }
 
 
-
-
-
-
-
-{- type alias Positioned a =
-    { a | x : Float, y : Float } -}
-
-
-{- type alias EmptyRecord a =
-    { a |  x : Float} -}
-{- type alias Positioned a =
-    { a | x : Float, y : Float } -}
-
-
-{- type alias EmptyRecord a =
-    { a |  x : Float} -}
-
-
--- type alias Asdf (Positioned {})
 reallyDumb : String
 reallyDumb = dummy
-
--- initialBuffer : Array Float
--- initialBuffer = Array.repeat bufferSize 0.0
 
 
 {- a helper function -}
@@ -103,97 +80,6 @@ foldn func initial count =
         initial
 
 
--- let's just hardcode sample rate for now (it's easier!)
-
-
-
-
-
-
--- updateBufferState : UserInput -> BufferState -> BufferState
--- updateBufferState userInput prevBufferState =
---
---     let
---
---         externalInputState : ExternalInputState
---         externalInputState =
---             { xWindowFraction = toFloat userInput.mousePosition.x / toFloat userInput.windowDimensions.width
---             , yWindowFraction = toFloat userInput.mousePosition.y / toFloat userInput.windowDimensions.height
---             , audioOn = userInput.audioOn
---             }
---         -- _ = Debug.log "externalInputState: " externalInputState
---
---         time = prevBufferState.time + sampleDuration
---
---         -- frequency = 40.0 + (xWindowFraction * 10000.0) -- how do we pass this in?
---
---         initialGraph = prevBufferState.graph
--- {-         _ = Debug.log "sampleCuration" sampleDuration
---         _ = Debug.log "updateBufferState time" time -}
---
---
---         -- surely we can do this without having to manually create a counter?
---         -- we can just iterate over the last buffer, and ignore values
---
---         prevBuffer = prevBufferState.buffer
---
---         initialBufferState : BufferState
---         initialBufferState =
---             { time = time
---             , graph = initialGraph
---             , buffer = prevBuffer
---             , bufferIndex = 0
---             , externalInputState = externalInputState
---             }
---
---         -- we must expose this as a public function
---         updateForSample {time, graph, buffer, bufferIndex} =
---             let
---                 newTime  = time + sampleDuration
---                 externalState =
---                     { time = newTime
---                     , externalInputState = externalInputState
---                     }
--- --                 _ = Debug.log "udpateForSample newTime" newTime
---                 newBufferIndex = bufferIndex + 1
--- --                 _ = Debug.log "newBufferIndex" newBufferIndex
--- --                 _ = Debug.log "value" value
---             in
---                 if
---                     externalInputState.audioOn == True
---                 then
---                     let
---                         -- this is pretty much all elm will do
---                         (newGraph, value) = updateGraph graph externalState
---                     in
---                         -- this will be done in JS land
---                         { time  = newTime
---                         , graph = newGraph
---                         , buffer = Array.set newBufferIndex value buffer
---                         , bufferIndex = newBufferIndex
---                         , externalInputState =  externalInputState
---                         }
---                 else
---                     { time  = newTime
---                     , graph = graph
---                     , buffer = Array.set newBufferIndex 0.0 buffer
---                     , bufferIndex = newBufferIndex
---                     , externalInputState =  externalInputState
---                     }
---     in
---         foldn updateForSample initialBufferState bufferSize
-
-
-{- squareA =
-    Generator
-        { id = "squareA"
-        , function = oscillator Square 444.0
-        , state =
-            { outputValue = 0.0  }
-        } -}
-
-
-
 destinationA : AudioNode
 destinationA =
     Destination
@@ -203,37 +89,6 @@ destinationA =
             { outputValue = 0.0 }
         }
 
-{- makeSquare id frequency =
-    Generator
-        { id = id
-        , function = oscillator Saw frequency
-        , state =
-            { outputValue = 0.0  }
-        } -}
-
-{- makeSin id frequency =
-    Generator
-        { id = id
-        , function = oscillator Sin frequency
-        , state =
-            { outputValue = 0.0  }
-        } -}
-
-
-
-
-{- lowPassNode id inputName =
-    FeedforwardProcessor
-        { id = id
-        , input = ID inputName
-        , function = simpleLowPassFilter
-        , state =
-            { outputValue = 0.0
---             , prevValues = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-            , prevValues =  List.repeat 2 0.0
---             , prevValues = [0.0, 0.0, 0,0]
-            }
-        } -}
 
 sinNode : String -> {frequency: Input, frequencyOffset: Input, phaseOffset: Input} -> AudioNode
 sinNode id {frequency, frequencyOffset, phaseOffset} =
@@ -288,11 +143,6 @@ commaHelper =
 
 
 
-
-
--- this could be like the "main" (JS expects this to be here)
-
-
 additiveSynthAudioGraph baseFrequency numOscillators =
     let
         getId n =
@@ -315,141 +165,18 @@ additiveSynthAudioGraph baseFrequency numOscillators =
 audioGraph2: ListGraph
 audioGraph2 =
     [ commaHelper
-
-{-     , sinNode "mod3'" {frequency = Value 200.0, frequencyOffset = Default, phaseOffset = Default}
-    , gainNode "mod3" {signal = ID "mod3'", gain = Value 70.0}
-
-    , sinNode "mod2'" {frequency = Value 800.0, frequencyOffset = ID "mod3" , phaseOffset = Default}
-    , gainNode "mod2" {signal = ID "mod2'", gain = Value 5000.0}
-
-    , sinNode "mod1'" {frequency = Value 400.0, frequencyOffset = ID "mod2", phaseOffset = Default}
-    , gainNode "mod1" {signal = ID "mod1'", gain = Value 200.0} -}
-
-{-     , sinNode "mod3'" {frequency = Value 200.0, frequencyOffset = Default, phaseOffset = Default}
-    , gainNode "mod3" {signal = ID "mod3'", gain = Value 70.0}
-
-    , sinNode "mod2'" {frequency = Value 800.0, frequencyOffset = ID "mod3" , phaseOffset = Default}
-    , gainNode "mod2" {signal = ID "mod2'", gain = Value 5000.0} -}
-
-{-     , sinNode "mod1" {frequency = Value 11025.0, frequencyOffset = Default, phaseOffset = Default}
---     , gainNode "mod1" {signal = ID "mod1'", gain = Value 200.0}
-    , sinNode "root1" {frequency = Value 11025.0, frequencyOffset = Default, phaseOffset = ID "mod1"} -}
-
---     , sinNode "mod2" {frequency = Value 345.0, frequencyOffset = Default, phaseOffset = Default}
-{-     , sinNode "mod1" {frequency = Value 11025.0, frequencyOffset = Value 666.0, phaseOffset = Default}
-    , sinNode "root1" {frequency = Value 11025.0, frequencyOffset = Default, phaseOffset = ID "mod1"} -}
---     , sinNode "root1" {frequency = Value 440.0, frequencyOffset = Default, phaseOffset = Default}
-
-{-     , sinNode "mod6" {frequency = Value 200.0, frequencyOffset = Value 666.0, phaseOffset = Default}
-    , sinNode "mod5" {frequency = Value 200.0, frequencyOffset = Value 666.0, phaseOffset = ID "mod6"}
-    , sinNode "mod4" {frequency = Value 200.0, frequencyOffset = Value 666.0, phaseOffset = ID "mod5"} -}
-
-    -- , sinNode "lfoRaw" {frequency = Value 0.25, frequencyOffset = Default, phaseOffset = Default}
-    -- , gainNode "lfoGain" {signal = ID "lfoRaw", gain = Value 20.0}
-    -- , adderNode "pitch" [Value 200.0, ID "lfoGain"]
-
-    -- , sinNode "mod3" {frequency = ID "pitch", frequencyOffset = Default, phaseOffset = Default}
     , sinNode "mod3" {frequency = Value 800.0, frequencyOffset = Default, phaseOffset = Default}
-    -- , sinNode "mod2" {frequency = ID "pitch", frequencyOffset = Default, phaseOffset = Default}
     , sinNode "mod2" {frequency = Value 600.0, frequencyOffset = Default, phaseOffset = ID "mod3"}
-
     , gainNode "mod1Frequency" {signal = ID "pitch", gain = Value 3.0}
-    -- , sinNode "mod1" {frequency = ID "mod1Frequency", frequencyOffset = Default, phaseOffset = Default }
-    -- , sinNode "mod1" {frequency = Value 400.0, frequencyOffset = Default, phaseOffset = Default }
     , sinNode "mod1" {frequency = Value 400.0, frequencyOffset = Default, phaseOffset = ID "mod2"}
-
-    -- , sinNode "root1" {frequency = ID "pitch", frequencyOffset = Default, phaseOffset = ID "mod1"}
     , sinNode "root1" {frequency = Value 200.0, frequencyOffset = Default, phaseOffset = ID "mod1"}
-    -- , sinNode "root1" {frequency = ID "mod1Frequency", frequencyOffset = Default, phaseOffset = Default}
-    -- , sinNode "root1" {frequency = ID "pitch", frequencyOffset = Default, phaseOffset = Default}
-    -- , sinNode "root1" {frequency = Value 200.0, frequencyOffset = Default, phaseOffset = Default}
-
-
     , destinationNode {signal = ID "root1"}
---     , destinationNode {signal = ID "lfoRaw"}
-
     ]
 
 
 audioGraph =
-    (additiveSynthAudioGraph 100.0 7)
+    (additiveSynthAudioGraph 100.0 6)
     ++ [ destinationNode {signal = ID "additiveSynth"} ]
-
-
-
-
--- I guess this can be done in JS, to avoid the user having to do it
--- testGraphDict : DictGraph
--- testGraphDict = toDict testGraph
-
-
-
--- initialBufferState : BufferState
--- initialBufferState =
---     { time = 0.0
---     , graph = testGraphDict
---     , buffer = initialBuffer
---     , bufferIndex = 0
---     , externalInputState =
---         { xWindowFraction = 0.0
---         , yWindowFraction = 0.0
---         , audioOn = False
---         }
-    -- }
-
-
-
-
-
-
-
-
-
--- we won't use this any port, rather we'll use userInputSignal as a port
--- bufferRequestWithUserInput : Signal UserInput
--- bufferRequestWithUserInput = Signal.sampleOn requestBuffer userInputSignal
-
-
-
--- sampleOn : Signal a -> Signal b -> Signal b
-
-
--- let's juoin
-
-
--- I'm not actually sure how to handle triggers
-
-
--- we can use map to merge different signals into one (using a record)
--- we also have a signal True from requestBuffer, which is really just a pulse (we don't care about the value)
-
-
-
-
-
--- bufferStateSignal : Signal BufferState
--- bufferStateSignal = Signal.foldp updateBufferState initialBufferState bufferRequestWithUserInput
-
-
-{- getSampleTime : Int -> Float -> Float
-getSampleTime bufferIndex bufferStartTime =
-    let
-        _ = Debug.log "bufferIndex" bufferStartTime
-
-    in
-        bufferStartTime + (toFloat bufferIndex * sampleDuration) -}
-
-
-
-
-
-
-
-
-
--- incoming port (no longer used)
--- port requestBuffer : Signal Bool
-
 
 
 main =
